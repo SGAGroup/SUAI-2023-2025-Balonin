378710bc487c0ecf6f571812ea98fc22bbc6f202
Nikita Nikiforov <111698816+NixNi@users.noreply.github.com>
2023-11-16 22:48:30 +0300

    Hotfix BaloningConverter

--- blender/ConverterToBaloning.py
+++ blender/ConverterToBaloning.py
@@ -15,100 +15,104 @@ class OBJECT_OT_run_script(bpy.types.Operator):
     bl_label = "Convert to Baloning"
     bl_options = {'REGISTER', 'UNDO'}
     
-    def getParameters(self, obj):
+    def get_parameters(self, obj):
         command = ""
-        textureAdditionParameters = ""
-        deltaX = 0
-        name = obj.name.replace(".","_")
-        if "cube" in name.lower(): command = "BoxGeometry(2, 2, 2)"
-        if "cylinder" in name.lower(): command = "CylinderGeometry(1, 1, 2)"
-        if "circle" in name.lower(): 
-            command = "CircleGeometry()"
-            deltaX = pi/2
-            textureAdditionParameters = ", side: THREE.DoubleSide"
-        if "sphere" in name.lower(): command = "SphereGeometry()"
-        if "cone" in name.lower(): command = "ConeGeometry(1, 2)"
-        if "torus" in name.lower(): 
-            command = "TorusGeometry(1, 0.25)"
-            deltaX = pi/2
-        if "plane" in name.lower(): 
-            command = "PlaneGeometry(2, 2)"
-            deltaX = pi/2
-            textureAdditionParameters = ", side: THREE.DoubleSide"
-        if "icosphere" in name.lower(): command = "IcosahedronGeometry(1, 1)"
-        return (command, textureAdditionParameters, deltaX, name)
+        texture_addition_parameters = ""
+        delta_x = 0
+        name = obj.name.replace(".", "_").lower()
+        
+        if "cube" in name: 
+            command, delta_x = "BoxGeometry(2, 2, 2)", 0
+        elif "cylinder" in name: 
+            command, delta_x = "CylinderGeometry(1, 1, 2, 32)", 0
+        elif "circle" in name: 
+            command, delta_x, texture_addition_parameters = "CircleGeometry()", pi/2, ", side: THREE.DoubleSide"
+        elif "sphere" in name: 
+            command, delta_x = "SphereGeometry(1, 32, 16)", 0
+        elif "cone" in name: 
+            command, delta_x = "ConeGeometry(1, 2, 32)", 0
+        elif "torus" in name: 
+            command, delta_x = "TorusGeometry(1, 0.25, 12, 48)", pi/2
+        elif "plane" in name: 
+            command, delta_x, texture_addition_parameters = "PlaneGeometry(2, 2)", pi/2, ", side: THREE.DoubleSide"
+        elif "icosphere" in name: 
+            command, delta_x = "IcosahedronGeometry(1, 1)", 0
+        
+        return command, texture_addition_parameters, delta_x, name
     
-    def getPSR(self, ob, name, deltaX):
+    def get_psr(self, ob, name, delta_x):
         message = ''
-        if ob.location.x or ob.location.z or ob.location.y:  message += name + '.position.set(' + str(round(ob.location.x, 4)) +', '+ str(round(ob.location.z, 4)) + ', '+ str(round(-ob.location.y, 4)) +');\n'
-        if ob.scale.x !=1 or ob.scale.z != 1 or ob.scale.y !=1: message += name + '.scale.set(' + str(round(ob.scale.x, 4)) +', '+ str(round(ob.scale.z, 4)) + ', '+ str(round(ob.scale.y, 4)) +');\n'
-        if ob.rotation_euler.x + deltaX or ob.rotation_euler.z or ob.rotation_euler.y: message += name + '.rotation.set(' + str(round(ob.rotation_euler.x + deltaX, 4)) +', '+ str(round(ob.rotation_euler.z, 4)) + ', '+ str(round(-ob.rotation_euler.y, 4)) +');\n'
-        return message;
+        if any(ob.location):  
+            message += f"{name}.position.set({round(ob.location.x, 4)}, {round(ob.location.z, 4)}, {round(-ob.location.y, 4)});\n"
+        if ob.scale != (1, 1, 1): 
+            message += f"{name}.scale.set({round(ob.scale.x, 4)}, {round(ob.scale.z, 4)}, {round(ob.scale.y, 4)});\n"
+        if any(ob.rotation_euler) or delta_x: 
+            message += f"{name}.rotation.set({round(ob.rotation_euler.x + delta_x, 4)}, {round(ob.rotation_euler.z, 4)}, {round(-ob.rotation_euler.y, 4)});\n"
+        return message
     
-    def getMaterialProp(self, obj, label):
+    def get_material_prop(self, obj, label):
         material = obj.material_slots[0].material
         nodes = material.node_tree.nodes
         principled = next(n for n in nodes if n.type == 'BSDF_PRINCIPLED')
         inp = principled.inputs[label] 
-        value = inp.default_value
-        return value;
-
-    def getColor(self, obj):
-        try:
-            value = self.getMaterialProp(obj, 'Base Color')
-            color = Color( (value[0], value[1], value[2]) )
-            return str(round(color.r, 4)) + ' ,' + str(round(color.g, 4)) + ' ,' + str(round(color.b, 4))
-        except:
-            return str(round(random.random(), 4)) + ', ' + str(round(random.random(), 4)) + ', ' + str(round(random.random(), 4))
-        
-    def getTransperency(self, obj):
-        try:
-            value = self.getMaterialProp(obj, 'Alpha')
-            if value < 1: return ', transparent: true, opacity: ' + str(round(value, 4))
-            else: return ''
-        except:
-            return ''
-    def getMetallic(self, obj):
-        try:
-            value = self.getMaterialProp(obj, 'Metallic')
-            if value > 0: return ', metallness: ' + str(round(value, 4))
-            else: return ''
-        except:
-            return ''
-    
-    def getRoughness(self, obj):
-        try:
-            value = self.getMaterialProp(obj, 'Roughness')
-            if value < 1: return ', roughness: ' + str(round(value, 4))
-            else: return ''
-        except:
-            return ''
-    
-    def getEmmission(self, obj):
-        try:
-            value = self.getMaterialProp(obj, 'Emission')
-            color = Color( (value[0], value[1], value[2]) )
-            if value[0] != 0 and value[1] != 0 and value[2] != 0: return ', emissive: ' + 'new THREE.Color('+ str(round(color.r, 4)) + ' ,' + str(round(color.g, 4)) + ' ,' + str(round(color.b, 4)) +')'
-            else: return ''
-        except:
-            return ''
-    
-    def addMaterial(self, obj, name, textureAdditionParameters, materials, materialsString):
-        try:
-            name = obj.material_slots[0].material.name
-            if "material" not in name.lower(): name += "Material"
-            if textureAdditionParameters != "": name += "DS"
-            if name in materials: return (name, materials, materialsString)
-            materialsString += 'var ' + name + ' = new THREE.MeshStandardMaterial({ color: new THREE.Color('+ self.getColor(obj) +')'+ textureAdditionParameters + self.getTransperency(obj) + self.getMetallic(obj) + self.getRoughness(obj) + self.getEmmission(obj) + ' });\n'
-            materials.append(name)
-            return (name, materials, materialsString)
-        except:
-            name = name + 'Material'
-            if textureAdditionParameters != "": name += "DS"
-            materialsString += 'var ' + name + ' = new THREE.MeshStandardMaterial({ color: new THREE.Color('+ self.getColor(obj) +')'+ textureAdditionParameters +' });\n'
-            materials.append(name)
-            return (name, materials, materialsString)
+        return inp.default_value
             
+    def get_color(self, obj):
+        try:
+            value = self.get_material_prop(obj, 'Base Color')
+            color = Color(value[:3])
+            return f"{round(color.r, 4)}, {round(color.g, 4)}, {round(color.b, 4)}"
+        except:
+            return f"{round(random.random(), 4)}, {round(random.random(), 4)}, {round(random.random(), 4)}"
+        
+    def get_transparency(self, obj):
+        try:
+            value = self.get_material_prop(obj, 'Alpha')
+            return f", transparent: true, opacity: {round(value, 4)}" if value < 1 else ''
+        except:
+            return ''
+    
+    def get_metallic(self, obj):
+        try:
+            value = self.get_material_prop(obj, 'Metallic')
+            return f", metalness: {round(value, 4)}" if value > 0 else ''
+        except:
+            return ''
+    
+    def get_roughness(self, obj):
+        try:
+            value = self.get_material_prop(obj, 'Roughness')
+            return f", roughness: {round(value, 4)}" if value < 1 else ''
+        except:
+            return ''
+    
+    def get_emission(self, obj):
+        try:
+            value = self.get_material_prop(obj, 'Emission')
+            color = Color(value[:3])
+            
+            return f", emissive: new THREE.Color({round(color.r, 4)}, {round(color.g, 4)}, {round(color.b, 4)})" if any(color) else ''
+        except:
+            return ''
+    
+    def add_material(self, obj, name, texture_addition_parameters, materials, materials_string):
+        try:
+            name = obj.material_slots[0].material.name.lower() if "material" not in name else name
+            name += "DS" if texture_addition_parameters else ""
+            
+            if name in materials: 
+                return name, materials, materials_string
+            
+            materials_string += f"var {name} = new THREE.MeshStandardMaterial({{ color: new THREE.Color({self.get_color(obj)}){texture_addition_parameters}{self.get_transparency(obj)}{self.get_metallic(obj)}{self.get_roughness(obj)}{self.get_emission(obj)} }});\n"
+            materials.append(name)
+            
+            return name, materials, materials_string
+        except:
+            name = name + 'material'
+            name += "DS" if texture_addition_parameters else ""
+            materials_string += f"var {name} = new THREE.MeshStandardMaterial({{ color: new THREE.Color({self.get_color(obj)}){texture_addition_parameters} }});\n"
+            materials.append(name)
+            return name, materials, materials_string
 
     def execute(self, context):
         scene = context.scene
@@ -116,19 +120,22 @@ class OBJECT_OT_run_script(bpy.types.Operator):
 
         message = ""
         names = []
-        materialsString = ""
+        materials_string = ""
         materials = []
         
         for ob in context.selected_objects:
-            command, textureAdditionParameters, deltaX, name = self.getParameters(ob)
-            materialName, materials, materialsString = self.addMaterial(ob, name, textureAdditionParameters, materials, materialsString)
-            message += 'var ' + name + 'Geometry' + ' = new THREE.'+command+';\n'
-            message += 'var ' + name + ' = new THREE.Mesh( '+name + 'Geometry'+', '+ materialName +' );\n'
-            message += self.getPSR(ob, name, deltaX)+'\n'
+            command, texture_addition_parameters, delta_x, name = self.get_parameters(ob)
+            material_name, materials, materials_string = self.add_material(ob, name, texture_addition_parameters, materials, materials_string)
+            
+            message += f"var {name}Geometry = new THREE.{command};\n"
+            message += f"var {name} = new THREE.Mesh({name}Geometry, {material_name});\n"
+            message += f"{self.get_psr(ob, name, delta_x)}\n"
             names.append(name)
-        message = materialsString +'\n' + message
-        message +='var out = new THREE.Group();\n'
-        message +='out.add('+', '.join(names)+')'
+        
+        message = f"{materials_string}\n{message}"
+        message += f"var out = new THREE.Group();\n"
+        message += f"out.add({', '.join(names)})"
+
         bpy.context.window_manager.clipboard = message
         self.report({'INFO'}, message)
         self.report({'INFO'}, "Script copied to clipboard!")

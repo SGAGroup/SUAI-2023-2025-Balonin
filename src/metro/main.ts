640a2b99e7d2387090b232722bb5fece7f7a28e4
Familex <trener2990@gmail.com>
2023-12-13 18:13:36 +0300

    pointerlockcontrols

--- src/metro/main.ts
+++ src/metro/main.ts
@@ -47,7 +47,18 @@ let Robot: THREE.Object3D;
 let scene: THREE.Scene = new THREE.Scene();
 let camera: THREE.Camera;
 let renderer: THREE.WebGLRenderer;
-let controls: THREE.OrbitControls;
+let controls: THREE.PointerLockControls;
+
+// balon ignore
+// controls globals
+let moveForward = false;
+let moveBackward = false;
+let moveLeft = false;
+let moveRight = false;
+let canJump = false;
+let prevTime = performance.now();
+const velocity = new THREE.Vector3();
+const direction = new THREE.Vector3();
 // balon ignore end
 
 function main() {
@@ -87,11 +98,41 @@ main();
 function render() {
   requestAnimationFrame(render);
 
+  console.log(velocity);
+
   if (F) {
     F = false;
   }
 
-  controls.update();
+  const time = performance.now();
+
+  const delta = (time - prevTime) / 1000;
+  velocity.x -= velocity.x * 10.0 * delta;
+  velocity.z -= velocity.z * 10.0 * delta;
+
+  velocity.y -= 9.8 * 100.0 * delta; // 100.0 = mass
+
+  direction.z = Number(moveForward) - Number(moveBackward);
+  direction.x = Number(moveRight) - Number(moveLeft);
+  direction.normalize(); // this ensures consistent movements in all directions
+
+  if (moveForward || moveBackward) velocity.z -= direction.z * 400.0 * delta;
+  if (moveLeft || moveRight) velocity.x -= direction.x * 400.0 * delta;
+
+  controls.moveRight(-velocity.x * delta);
+  controls.moveForward(-velocity.z * delta);
+  // controls.getObject().position.y += velocity.y * delta; // new behavior
+
+  if (controls.getObject().position.y < 10) {
+    velocity.y = 0;
+    controls.getObject().position.y = 10;
+
+    canJump = true;
+  }
+
+  prevTime = time;
+
+  // controls.update();
   renderer.render(scene, camera);
 }
 
@@ -2206,10 +2247,7 @@ function CreateScene(WC: number, HC: number) {
     document.getElementById('wCanvas')?.appendChild(renderer.domElement);
     renderer.setSize(WC, HC);
     // установим модуль управления камерой
-    controls = new THREE.OrbitControls(camera, renderer.domElement);
-    controls.rotateSpeed = 1.0;
-    controls.zoomSpeed = 1.2;
-    controls.panSpeed = 0.8;
+    setupcontrols(scene);
 
     // источники света
     scene.add(new THREE.AmbientLight(0x555555, 2));
@@ -2238,14 +2276,69 @@ function CreateScene(WC: number, HC: number) {
     document.body.appendChild(renderer.domElement);
   }
 }
-// balon block end
 
-/*
-    {{html
-    <!DOCTYPE html>
-    <head>
-     <script SRC="http://livelab.spb.ru/x3d/three.min.js"></script>
-     <script SRC="http://livelab.spb.ru/x3d/OrbitControls.js"></script>
-    </head>
-    html}}
-  */
+function setupcontrols(scene: THREE.Scene) {
+  controls = new THREE.PointerLockControls(camera, renderer.domElement);
+  scene.add(controls.getObject());
+
+  const onKeyDown = function (event: KeyboardEvent) {
+    switch (event.code) {
+      case 'ArrowUp':
+      case 'KeyW':
+        moveForward = true;
+        break;
+
+      case 'ArrowLeft':
+      case 'KeyA':
+        moveLeft = true;
+        break;
+
+      case 'ArrowDown':
+      case 'KeyS':
+        moveBackward = true;
+        break;
+
+      case 'ArrowRight':
+      case 'KeyD':
+        moveRight = true;
+        break;
+
+      case 'Space':
+        if (canJump === true) velocity.y += 350;
+        // canJump = false;
+        break;
+    }
+  };
+
+  const onKeyUp = function (event: KeyboardEvent) {
+    switch (event.code) {
+      case 'ArrowUp':
+      case 'KeyW':
+        moveForward = false;
+        break;
+
+      case 'ArrowLeft':
+      case 'KeyA':
+        moveLeft = false;
+        break;
+
+      case 'ArrowDown':
+      case 'KeyS':
+        moveBackward = false;
+        break;
+
+      case 'ArrowRight':
+      case 'KeyD':
+        moveRight = false;
+        break;
+    }
+  };
+
+  document.addEventListener('keydown', onKeyDown);
+  document.addEventListener('keyup', onKeyUp);
+
+  document.addEventListener('click', function () {
+    controls.lock();
+  });
+}
+// balon block end
--- src/types.ts
+++ src/types.ts
@@ -1,2 +1,3 @@
 export * from 'three/addons/controls/OrbitControls.js';
+export * from 'three/addons/controls/PointerLockControls.js';
 export * from 'three';
